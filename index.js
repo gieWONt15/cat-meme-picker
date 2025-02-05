f679cadf542c8fcb098de389e53db0facbef2a43
Paweł <pawdyslaw@gmail.com>
2025-02-05 10:26:18 +0100

    test

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function renderCat(){
     memeModalInner.innerHTML =  `
         <img 
         class="cat-img" 
-        src="images/${catObject.image}"
+        src="./images/${catObject.image}"
         alt="${catObject.alt}"
         >
         `
